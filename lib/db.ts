0007b9d2d7460674e6b34dc152be253668e2f751
Ashok <3ddesigner@Ashok-MacBook.local>
2026-06-09 12:57:10 +0530

    Use online MySQL database

--- lib/db.ts
+++ lib/db.ts
@@ -1,23 +1,13 @@
 import mysql from "mysql2/promise";
 
-declare global {
-  var mysqlPool: mysql.Pool | undefined;
-}
-
-const pool =
-  global.mysqlPool ||
-  mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "klassic",
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-  });
-
-if (process.env.NODE_ENV !== "production") {
-  global.mysqlPool = pool;
-}
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
 export default pool;
